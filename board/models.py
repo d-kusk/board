5d91256d84cb369a2bcb5f8058a8b51be0fb5997
Daisuke KONISHI <skydai1151@gmail.com>
2018-10-30 21:25:14 +0900

    update list view
    create detail view

--- board/models.py
+++ board/models.py
@@ -8,7 +8,7 @@ class Board(models.Model):
 
     title = models.CharField(max_length=255)
     content = models.TextField()
-    pub_date = models.DateField(default=timezone.now)
+    created_at = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.title
